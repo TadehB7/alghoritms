d2e4ff4027a80aa6eb210701e320208fc5d6ac16
TadehB7 <99886944+TadehB7@users.noreply.github.com>
2022-10-09 18:41:14 +0400

    SumOfTwoIntegersIsEqualToTheGivenValue
    SumOfTwoIntegersIsEqualToTheGivenValue

---
+++ Determine_if_the_sum_of_two_integers_is_equal_to_the_given_value/SumOfTwoIntegersIsEqualToTheGivenValue.cs
@@ -0,0 +1,21 @@
+﻿namespace Alghoritms.Determine_if_the_sum_of_two_integers_is_equal_to_the_given_value
+{
+    public static class SumOfTwoIntegersIsEqualToTheGivenValue
+    {
+        public static bool SumOfTwoIntegersIsEqualToTheGivenValueFunction(int[] array, int value)
+        {
+            HashSet<int> set = new HashSet<int>();
+
+            foreach (var item in array)
+            {
+                if (set.Contains(value - item))
+                {
+                    return true;
+                }
+                set.Add(item);
+            }
+
+            return false;
+        }
+    }
+}
